e6090a3715960ce3ffccfb65c374e42e8de2a994
Gursel Safran <Gursel_safran@hotmail.com>
2022-04-11 07:47:50 +0300

    Mvc Blog Projesi
    Mvc katmanlı mimari ile blog sitesi yapımı ilk aşama

---
+++ MVCBlog/DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        List<T> GetListAll();
+        T GetById(int id);
+    }
+}
---
+++ MVCBlog/MVCBlog/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
